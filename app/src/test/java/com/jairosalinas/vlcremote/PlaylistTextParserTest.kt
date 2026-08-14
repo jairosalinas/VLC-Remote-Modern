f3994591380add629fbf8313b32dee3b2f271872
jairosalinas <HECTORJS2000@GMAIL.COM>
2026-08-14 00:56:07 -0500

    Add playlist parser unit tests

---
+++ app/src/test/java/com/jairosalinas/vlcremote/PlaylistTextParserTest.kt
@@ -0,0 +1,28 @@
+package com.jairosalinas.vlcremote
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PlaylistTextParserTest {
+    @Test fun ignoresM3uCommentsAndBlankLines() {
+        val result = PlaylistTextParser.parse(
+            sequenceOf("#EXTM3U", "", "  http://example.test/a.ts  ", "#EXTINF:-1,Canal", "http://example.test/b.ts")
+        )
+        assertEquals(listOf("http://example.test/a.ts", "http://example.test/b.ts"), result)
+    }
+
+    @Test fun parsesPlsFileEntries() {
+        val result = PlaylistTextParser.parse(
+            sequenceOf("[playlist]", "File1=http://example.test/a", "Title1=A", "File2=http://example.test/b")
+        )
+        assertEquals(
+            listOf("[playlist]", "http://example.test/a", "Title1=A", "http://example.test/b"),
+            result
+        )
+    }
+
+    @Test fun preservesLocalAndNetworkPaths() {
+        val result = PlaylistTextParser.parse(sequenceOf("D:\\Media\\movie.mkv", "smb://server/media/song.mp3"))
+        assertEquals(listOf("D:\\Media\\movie.mkv", "smb://server/media/song.mp3"), result)
+    }
+}
